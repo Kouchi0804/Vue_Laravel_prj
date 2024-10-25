5c7285cd57838b5c2ce8488320b448bf82059d62
Kouchi0804 <kochi.susu03mu@gmail.com>
2024-10-25 15:04:24 +0900

    Goodsモデルにfillableを追加

--- Vue_Laravel_prj/app/Models/Goods.php
+++ Vue_Laravel_prj/app/Models/Goods.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Goods extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'goods_title',
+        'goods_price',
+        'goods_stock',
+        'goods_comment',
+        'goods_img',
+    ];
 }
