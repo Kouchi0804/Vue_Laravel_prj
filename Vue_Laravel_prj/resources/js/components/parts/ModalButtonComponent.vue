6ddc7c4b9f208d3ea325bc863bd707bfd698cbb6
Kouchi0804 <kochi.susu03mu@gmail.com>
2024-12-05 17:22:06 +0900

    モーダルコンポーネント追加

---
+++ Vue_Laravel_prj/resources/js/components/parts/ModalButtonComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref, reactive  } from 'vue';
+  import Sample from '@/components/parts/ModalContentComponent.vue';
+
+  const sampleModalVisible = ref(false);
+
+  const showSampleModal = () => {
+    sampleModalVisible.value = true;
+  };
+
+  const closeSampleModal = () => {
+    sampleModalVisible.value = false;
+  };
+
+
+</script>
+
+<template>
+  <div>
+    <button type="button" v-on:click="showSampleModal()" class="btn btn-danger">モーダル</button>
+    <Sample :isVisible="sampleModalVisible" @close="closeSampleModal" />
+  </div>
+</template>
+  
+
+  
+<style scoped lang='sass'>
+  
+</style>
