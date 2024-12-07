14973573be6998a93fa0b09ea17e9675ff0cb55c
Kouchi0804 <kochi.susu03mu@gmail.com>
2024-12-07 21:16:24 +0900

    商品詳細コンポーネントにteleport要素のモーダル追加

--- Vue_Laravel_prj/resources/js/components/peges/GoodsShowComponent.vue
+++ Vue_Laravel_prj/resources/js/components/peges/GoodsShowComponent.vue
@@ -14,6 +14,11 @@
 
     //data      --------------------------------------------------
     const goodslist = reactive({});
+    
+    //モーダルの表示状態
+    const isModalVisible = ref(false);
+    // 拡大表示する画像のURL
+    const modalImageSrc = ref('');
 
     //methods   --------------------------------------------------
     const getGoods = () =>{
@@ -58,7 +63,19 @@
             }
         }
     };
+    
+    // マウスオンでモーダルを表示
+    const showModal = (imageSrc) => {
+        modalImageSrc.value = imageSrc;
+        isModalVisible.value = true;
+    };
 
+    // マウスアウトでモーダルを非表示
+    const hideModal = () => {
+        isModalVisible.value = false;
+    };
+
+    //Mounted   --------------------------------------------------
     onMounted(() => {
         getGoods();
     });
@@ -70,7 +87,17 @@
         <div class="container">
             <form>
                 <div class="form_box" >
-                    <img alt="Vue logo" :src="`${goodslist.goods_img}`" >
+                    <img 
+                        alt="Vue logo" 
+                        :src="`${goodslist.goods_img}`" 
+                        @mouseover="showModal(`${goodslist.goods_img}`)"
+                        @mouseout="hideModal"
+                    >
+                    <teleport to="body">
+                    <div v-if="isModalVisible" class="modal-overlay" @mousemove="hideModal">
+                        <img :src="modalImageSrc" class="modal-image" alt="Expanded Image" />
+                    </div>
+                    </teleport>
                     <div class="form_content">
                         <div class="form-group">
                             <label for="goods_title">商品名</label>
@@ -84,7 +111,10 @@
                             <label for="goods_title">商品価格</label>
                             <input type="text" class="form-control" id="goods_title" aria-describedby="goods_title" placeholder="商品名" v-bind:value="goodslist.goods_price" disabled>
                         </div>
-                        <div ><button type="button" v-on:click="deleteGoodsConfirm()" class="btn btn-danger">商品を削除</button><Modal/></div>
+                        <div >
+                            <button type="button" v-on:click="deleteGoodsConfirm()" class="btn btn-danger">商品を削除</button>
+                            <!-- <Modal/> -->
+                        </div>
                         
                         <!-- <button type="button" v-on:click="moveBox(goodslist.id)" class="btn btn-primary test_btn" :id="'item_' + goodslist.id">test</button> -->
                     </div>
@@ -105,6 +135,39 @@
     .form_content{
         width: 50%;
         position: relative;
+    }
+
+    .image-gallery img {
+        width: 150px;
+        height: 150px;
+        margin: 10px;
+        cursor: pointer;
+        transition: transform 0.3s;
+    }
+    .image-gallery img:hover {
+    transform: scale(1.1);
+    }
+
+    /* モーダルのオーバーレイスタイル */
+    .modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+    }
+
+    /* モーダル内の画像スタイル */
+    .modal-image {
+    max-width: 90%;
+    max-height: 90%;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+    border-radius: 5px;
     }
 
 </style>
